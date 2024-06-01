d491571d74ec82c7f5fbb64c038d1c8dbb2b9d68
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2024-06-01 22:43:37 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/me/ezzedine/me/xpenser/mobile/xpenser_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package me.ezzedine.me.xpenser.mobile.xpenser_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/account/currency.dart
@@ -0,0 +1,22 @@
+class Currency {
+  final String code;
+  final String symbol;
+  final String name;
+
+  Currency({
+    required this.code,
+    required this.symbol,
+    required this.name
+  });
+
+  factory Currency.fromJson(Map<String, dynamic> json) {
+    return switch(json) {
+      {
+      'code': String code,
+      'symbol': String symbol,
+      'name': String name
+      } => Currency(code: code, symbol: symbol, name: name),
+      _ => throw const FormatException('Failed to load currency')
+    };
+  }
+}
